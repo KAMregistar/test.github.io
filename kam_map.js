dd79683593ed0684d1936aa86504e6812ff54881
JellieDe <168730968+KAMregistar@users.noreply.github.com>
2025-10-03 12:09:34 +0200

    Create kam_map.js

---
+++ kam_map.js
@@ -0,0 +1,5 @@
+const MAP_FIELDS = {
+  "Glavni stvarni naslov": "http://www.registar.kam.hr/Elements/st/1234",
+  "Usporedni stvarni naslov": "http://www.registar.kam.hr/Elements/st/1235",
+  "Napomena o stvarnom naslovu": "http://www.registar.kam.hr/Elements/st/1236"
+};
